44a3163caec6c09ac155349b2a32a12bafec2c59
Bryce Bjork <brycedbjork@gmail.com>
2019-08-01 21:27:08 -0400

    blah

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import controller from "./controller";
 
-export default app => {
+export default (app: any) => {
   app.route("/about").get(controller.about);
   app.route("/job/create").get(controller.job.create);
   // app.route("/fetchLocations").post(controller.fetchLocations);
