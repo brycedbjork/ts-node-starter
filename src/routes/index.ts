9a208faa9bbb84fe37f97d549ec2489b21d41aed
Bryce Bjork <brycedbjork@gmail.com>
2019-10-09 21:45:16 -0400

    routes for user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,12 @@
 import about from "./about";
+import user from "../services/user";
 import express from "express";
 
 export default (app: express.Application) => {
   app.route("/about").get(about);
+  app.route("/user/:uid").get(user.get);
+  app.route("/user/:uid").put(user.create);
+  app.route("/user/:uid").delete(user.delete);
+  app.route("/user/:uid").patch(user.update);
+  app.route("/user/locate").post(user.locate);
 };
